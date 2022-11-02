3affe76bb3e1ca1518b8eb0e0e9d2b1b8d9cb8cd
ShahriyarHridoy <35798678+ShahriyarHridoy@users.noreply.github.com>
2022-11-02 06:03:56 +0600

    try to deploy in heroku

--- project/project/settings.py
+++ project/project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -160,3 +161,6 @@ EMAIL_USE_TLS = True
 
 PASSWORD_RESET_TIMEOUT = 14400
 ...
+
+#Active Django-Heroku
+django_heroku.settings(locals())
